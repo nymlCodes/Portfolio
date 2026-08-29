ffe92d101eaf8b93d625d484604bb1500bb6906e
neya-mul <neyamulislam946@gmail.com>
2026-08-29 18:47:08 +0600

    bug fixed

--- src/components/layers/Contact.jsx
+++ src/components/layers/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
   }
 
   // Handle form submit - opens email client with pre-filled data
-  async function handeSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
     try {
